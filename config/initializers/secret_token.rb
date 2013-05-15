67ebf04b9f11584cca8cf859d23c6981738d459e
matts134 <mshopsin@gmail.com>
2013-05-15 12:15:28 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EarthquakeChallenge::Application.config.secret_token = '7e2ca9a74d67a19f8048281bc0beadfc0c1f7e65c1dd572837ac249df8df4f4e76103fb4dd142c70e4353cf24dcbbfed96c343386ab7d6732a7023abbe0914cd'
